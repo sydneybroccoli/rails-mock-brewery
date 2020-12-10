d20d18629e1b1c5e6ddc2ab264d7649fb6c5fbe0
Sydney Barovsky <sbarovsky@gmail.com>
2020-12-09 22:33:51 -0800

    added pages via routes and created the corresponding views and methods in the pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   mount Lockup::Engine, at: '/lockup'
 
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'beers', to: 'pages#beers'
+  get 'events', to: 'pages#events'
+  get 'merch', to: 'pages#merch'
+  get 'contact', to: 'pages#contact'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
